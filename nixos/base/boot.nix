3bf2cc89ec9d3ed5d89447f127696d416524c8f2
Liam Dyer <liamcdyer@gmail.com>
2024-05-27 01:29:34 -0400

    feat: switch from grub to systemd-boot

--- nixos/base/boot.nix
+++ nixos/base/boot.nix
@@ -4,23 +4,8 @@
   boot = {
     loader = {
       efi.canTouchEfiVariables = true;
-      timeout = 30;
-      grub = {
-        enable = false;
-        efiSupport = true;
-        device = "/dev/disk/by-uuid/df46494f-a7c5-4959-8007-c1304b801413";
-        theme = pkgs.fetchFromGitHub {
-          owner = "OliveThePuffin";
-          repo = "yorha-grub-theme";
-          rev = "4d9cd37baf56c4f5510cc4ff61be278f11077c81";
-          hash = "sha256-ye7EoL9gyjNKPVFfJvHopxREUhe8hpHZEeytAtlEd3c=";
-          # TODO: check that this works
-          postFetch = ''
-            mv $out/yorha-2560x1440/* $out/
-            cd $out && rm -r yorha* README.md preview.png
-          '';
-        };
-      };
+      timeout = 5;
+      systemd-boot.enable = true;
     };
 
     # Disable waiting for network because it takes 10s and we don't need it
