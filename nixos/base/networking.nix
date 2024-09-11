d4429e564b569973ab602dee32857eb62170ee97
Liam Dyer <liamcdyer@gmail.com>
2024-09-11 15:59:14 -0400

    feat: use 1.1.1.1 as nameserver

--- nixos/base/networking.nix
+++ nixos/base/networking.nix
@@ -6,7 +6,10 @@
   services.blueman.enable = true;
 
   # Internet
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    insertNameservers = [ "1.1.1.1" "1.0.0.1" ];
+  };
   # Causes long boots and hangs on update
   # https://github.com/NixOS/nixpkgs/issues/180175#issuecomment-1473408913
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
