37d555f82cbd7a8311dc9754e12806c93b97d5b5
Liam Dyer <liamcdyer@gmail.com>
2024-10-18 22:12:38 -0400

    feat: fat mpv cache size

--- home-manager/apps/video.nix
+++ home-manager/apps/video.nix
@@ -3,5 +3,12 @@
 {
   home.packages = with pkgs; [ vlc ];
 
-  programs.mpv.enable = true;
+  programs.mpv = {
+    enable = true;
+    config = {
+      profile = "gpu-hq";
+      cache = "yes";
+      demuxer-max-bytes = "5000000KiB"; # 5GB
+    };
+  };
 }
