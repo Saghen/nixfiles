cde4adfbddb86d04dc52f15558fc5f452f17b901
Liam Dyer <liamcdyer@gmail.com>
2025-10-18 10:39:02 -0400

    feat: pipewire resample quality + fixed sample rates

--- nixos/base/sound.nix
+++ nixos/base/sound.nix
@@ -7,5 +7,25 @@
     pulse.enable = true; # PulseAudio compatibility
     alsa.enable = true; # ALSA compatibility
     alsa.support32Bit = true; # Wiki says to do it and I don't ask questions
+
+    extraConfig.pipewire = {
+      # Auto switches between sample rates based on current audio
+      "10-clock-rate" = {
+        "context.properties" = {
+          "default.clock.rate" = "48000";
+          "default.clock.allowed-rates" = [
+            "44100"
+            "48000"
+          ];
+        };
+      };
+
+      # Higher resampling quality makes 44.1khz -> 48khz audibly transparent
+      "20-resample-quality" = {
+        "stream.properties" = {
+          "resample.quality" = "10";
+        };
+      };
+    };
   };
 }
